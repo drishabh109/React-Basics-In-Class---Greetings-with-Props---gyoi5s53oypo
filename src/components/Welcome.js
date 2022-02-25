e2d5ac7d0e9325bcca86bf0123abc323dbf67227
drishabh109 <96375523+drishabh109@users.noreply.github.com>
2022-02-25 12:06:19 +0530

    second

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Welcome(prop) {
+  const [name, setName] = useState();
+  const pro = () => {
+    setName((name) => !name);
+  };
   return (
     <>
-      <h1>Hey ! {prop.name}</h1>
+      <h1>Hey !</h1>
+      <button onclick={pro}>click</button>
+      {name && <h1>{prop.name}</h1>}
       <h2>Welcome to Newton School.</h2>
     </>
   );
